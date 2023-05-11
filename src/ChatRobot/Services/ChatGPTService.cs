66b050775fe23d7a2482f4002274c386e12c6946
licj <396638509@qq.com>
2023-05-11 11:30:36 +0800

    1

--- src/ChatRobot/Services/ChatGPTService.cs
+++ src/ChatRobot/Services/ChatGPTService.cs
@@ -1,35 +1,32 @@
 ﻿
-using Rystem.OpenAi;
-using Rystem.OpenAi.Chat;
+
+using OpenAI_API;
+using OpenAI_API.Chat;
 
 namespace ChatRobot.Services
 {
     public class ChatGPTService : IChatGPTService
     {
-        private IOpenAiChat openAiApi;
+        private IOpenAIAPI openAiApi;
         private ILogger logger;
+        private Conversation conversation;
 
         public ChatGPTService(string secret, ILogger loggger)
         {
             Check.IsNotNull(secret, nameof(secret));
             this.logger = loggger;
-            OpenAiService.Instance.AddOpenAi(setting => setting.ApiKey = secret, "NoDI");
-            openAiApi = OpenAiService.Factory.CreateChat("chat");
-            Check.IsNotNull(openAiApi, "openAIAPI");
+            openAiApi = new OpenAIAPI(secret);
+            conversation = openAiApi.Chat.CreateConversation();
         }
 
         public async Task<string> Send(string chat, string message)
         {
             this.logger.LogInformation("sendMessage to ai:" + message);
-            var results = await openAiApi
-                .Request(new ChatMessage { Role = ChatRole.User, Content = message })
-                .WithModel(ChatModelType.Gpt35Turbo)
-                .WithTemperature(1)
-                .ExecuteAsync();
 
-            var content = results.Choices[0].Message.Content;
-            this.logger.LogInformation("sendMessage to ai response:" + content);
-            return content;
+            conversation.AppendUserInput(message);
+            var response = await conversation.GetResponseFromChatbotAsync();
+            this.logger.LogInformation("sendMessage to ai response:" + response);
+            return response;
         }
     }
 }
